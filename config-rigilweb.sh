739b19396695d4efafed3b8ff60c6fbc0b40e961
Michael Ivanov <exception0x876@gmail.com>
2025-01-02 18:35:54 +1300

    Update company name

---
+++ config-rigilweb.sh
@@ -0,0 +1,8 @@
+export COMPANY="Rigil Web Technologies Limited"
+export C_SHORT="rigilweb"
+
+export INSTALLATION_REPORT_URL='https://rigilweb.nz/api/data_center_metrics/v1/installimage_installation_reports'
+
+export DNSRESOLVER=("185.12.64.1" "185.12.64.2")
+export DNSRESOLVER_V6=("2a01:4ff:ff00::add:1" "2a01:4ff:ff00::add:2")
+export NTPSERVERS=("0.au.pool.ntp.org" "1.au.pool.ntp.org" "2.au.pool.ntp.org")
